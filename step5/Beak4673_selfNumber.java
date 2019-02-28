3d278597d80e17468e1c0b74663456b07b393b11
SOLT <44250982+kwangho-park@users.noreply.github.com>
2019-02-28 21:44:24 +0900

    Add files via upload

---
+++ step5/Beak4673_selfNumber.java
@@ -0,0 +1,35 @@
+/*
+ * 셀프넘버의 그룹을 구하는 문제
+ *  - 10000보다 작거나 같은 셀프넘버를 한 줄에 하나씩 출력
+ *  
+ * 0 조건
+ *  - 양의정수 n
+ *  - d(n) = n + n의 각 자리수의 합
+ * 
+ * 1. 
+ * 
+ * 
+ *    
+ * */
+
+package step5;
+
+import java.util.Scanner;
+
+public class Beak4673_selfNumber {
+
+	public static void main(String[] args) {
+		
+		
+		
+	} // main() END
+	
+	
+	
+	public static void selfNumber(int inputInt) {
+		
+
+		
+	} // selfNumber() END
+
+} // Beak4673_selfNumber END
