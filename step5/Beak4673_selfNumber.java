786f3e04c6567fb59b56ae0a1f0345475140032f
SOLT <44250982+kwangho-park@users.noreply.github.com>
2019-03-03 19:15:32 +0900

    Add files via upload

--- step5/Beak4673_selfNumber.java
+++ step5/Beak4673_selfNumber.java
@@ -14,22 +14,50 @@
 
 package step5;
 
-import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
-public class Beak4673_selfNumber {
 
-	public static void main(String[] args) {
+// 스트림 학습 및 정리노트 작성 (노란색의 뒷장에 작성)
+
+public class Beak4673_selfNumber{
+
+	public static void main(String[] args) throws IOException{
 		
 		
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		
+		int n = 10001;
+		int sum = 0;
+		int i,x;
+		
+		boolean[] SN = new boolean[n];
+		
+		
+		for(i=1 ; i<n ; i++) {
+			x = i;
+			sum = i;
+			
+			while(x != 0) {
+				sum = sum + (x % 10);
+				x = x / 10;
+			}
+			
+			if(sum <= 10000) {
+				SN[sum] = true;
+			}
+		}
+		
+		i=1;
+		
+		while(i<=10000) {
+			if(SN[i] != true) {
+				bw.write(i+"\n");
+			}
+			i++;
+		}
+		bw.flush();		
+
 	} // main() END
-	
-	
-	
-	public static void selfNumber(int inputInt) {
-		
-
-		
-	} // selfNumber() END
-
 } // Beak4673_selfNumber END
